f4661db4626267dcd69f4a89fa837ad5a75f4f64
Emo <vineet.biscuit@gmail.com>
2020-05-20 16:38:53 +0530

    add merge sort(unfinished)

---
+++ merge_sort.py
@@ -0,0 +1,14 @@
+def merge_sort(mylist):
+    '''(list) -> list
+    
+    Returns a sorted version of the input list mylist.
+
+    >>> merge_sort([2,10,15,5,4,20,6,8,9, 'a'])
+    Invalid input, list elements should be of the same type
+
+    >>> merge_sort([2,10,15,5,4,20,6,8,9])
+    [2, 4, 5, 6, 8, 9, 10, 15, 20]
+    
+    '''
+
+
